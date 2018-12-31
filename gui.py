950f317963fb3a3da9a2fe3145575cfbbc564a38
Valentin Weyer <valentin.weyer@gmail.com>
2018-12-31 14:14:55 +0100

    First Test-code with date

--- gui.py
+++ gui.py
@@ -5,6 +5,7 @@ import Adafruit_DHT
 
 from clock import *
 from temperature import *
+from date import *
 
 root = tk.Tk()
 root.wm_title("Wetterstation")
@@ -14,5 +15,8 @@ clock.grid(row=0, column=0, padx=10, pady=10)
 
 temperature = TemperatureWidget(root)
 temperature.grid(row=1, column=0, padx=10, pady=10)
+
+date = DateWidget(root)
+date.grid(row=0, column=4, padx=10, pady=10)
       
 root.mainloop()
